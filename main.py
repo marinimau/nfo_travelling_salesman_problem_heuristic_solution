e70ff484e6064e0ded3ff76c5c234cfeb1efeb8e
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2021-08-25 18:25:53 +0200

    Test docplex

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from docplex.mp.model import Model
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+citta = 6
 
+Costs = [[999., 3.71, 9.6, 8.03, 6.17, 11.12],
+         [9.71, 999., 6.54, 5.2, 3.08, 7.42],
+         [28.06, 19.54, 999., 4.66, 1999.5, 7.57],
+         [24.03, 15.2, 1.66, 999., 6.12, 7.25],
+         [18.17, 9.08, 3.5, 2.12, 999., 6.22],
+         [29.12, 19.42, 6.57, 9.25, 15.22, 999.]]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+m = Model(name='heuristic', log_output=True)
 
+x = m.binary_var_matrix(citta, citta)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+m.minimize(m.sum(m.sum(Costs[i][j] * x[i][j]) for j in range(1, citta)) for i in range(1, citta))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+m.add_constraint(m.sum(x[i][j] == 1 for j in range(citta)) for i in range(citta))
+m.add_constraint(m.sum(x[i][j] == 1 for i in range(citta)) for j in range(citta))
+
+msol = m.solve()
+
+m.report()
