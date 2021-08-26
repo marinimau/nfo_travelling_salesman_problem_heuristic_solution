2ad80d7ba661ebf717cfa4364ce45551ca40ff2a
marinimau <mauro.marini97@gmail.com>
2021-08-26 15:51:50 +0200

    refactoring

--- subpath_finder.py
+++ subpath_finder.py
@@ -1,22 +1,48 @@
-# Get the next non visited node
-# Args:
-#   visited (boolean list) -> List that track which nodes have been visited
-# Returns:
-#   (int) -> -1 if all nodes have been visited or the index of the first node that hasn't been visited
-def get_next_node(visited):
+"""
+Copyright (c) 2021 Mauro Marini, Andrea Piras
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+def get_subsequent_node(visited):
+    """
+    get the subsequent node
+    :param visited: a boolean list tracking the nodes which have been visited
+    :return: -1 if all nodes have been visited or the index of the first node that hasn't been visited
+    """
     for idx, val in enumerate(visited):
         if not val:
             return idx
     return -1
 
 
-# Return all the sub paths
-# Args:
-#   df (pd.dataframe) -> the dataframe with the solution of the model
-#   nodes (int) -> the number of the nodes
-# Returns:
-#   (list of list) -> the list of sub paths
 def get_paths(df, nodes):
+    """
+    return all the sub paths
+    :param df: a Pandas dataframe that contains the solution of the model
+    :param nodes: the number of the nodes
+    :return: a list of sub paths (list of list)
+    """
     visited = [False] * nodes
     paths = []
     path = []
@@ -24,23 +50,23 @@ def get_paths(df, nodes):
     visited[df['start'].iloc[0]] = True
     path.append(df['start'].iloc[0] + 1)
     # Take the endpoint of the arc
-    next = df['end'].iloc[0]
-    while next != -1:
+    subsequent = df['end'].iloc[0]
+    while subsequent != -1:
         # Set the start node to the visited and add it to the path
-        visited[df['start'].iloc[next]] = True
-        path.append(df['start'].iloc[next] + 1)
+        visited[df['start'].iloc[subsequent]] = True
+        path.append(df['start'].iloc[subsequent] + 1)
         # If the end of the arc is not visited
-        if not visited[df['end'].iloc[next]]:
-            # Change the next node with the end of the arc
-            next = df['end'].iloc[next]
+        if not visited[df['end'].iloc[subsequent]]:
+            # Change the subsequent node with the end of the arc
+            subsequent = df['end'].iloc[subsequent]
         # If the end of the arc is visited
         else:
             # Add the end node to the path
-            path.append(df['end'].iloc[next] + 1)
+            path.append(df['end'].iloc[subsequent] + 1)
             # Add the sub path to the paths list
             paths.append(path)
             # Start a new path
             path = []
-            # Get the next node not visited
-            next = get_next_node(visited)
+            # Get the subsequent node not visited
+            subsequent = get_subsequent_node(visited)
     return paths
--- utils.py
+++ utils.py
@@ -1,7 +1,38 @@
+"""
+Copyright (c) 2021 Mauro Marini, Andrea Piras
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 import numpy
 
 
 def convert_row(d, nodes):
+    """
+    Convert row
+    :param d:
+    :param nodes:
+    :return:
+    """
     row = int(int(d['name'][1:]) / nodes) + 1
     col = int(int(d['name'][1:]) % nodes)
     if col == 0:
@@ -11,6 +42,12 @@ def convert_row(d, nodes):
 
 
 def convert_dataframe_names(df, nodes):
+    """
+    Convert dataframe names
+    :param df:
+    :param nodes:
+    :return:
+    """
     start = []
     end = []
     for index, d in df.iterrows():
@@ -23,13 +60,13 @@ def convert_dataframe_names(df, nodes):
     return df
 
 
-# Get the decision variable x in matrix form
-# Args:
-#   df (pd.dataframe) -> the dataframe with the solution of the model
-#   nodes (int) -> the number of the nodes
-# Returns:
-#   (ndarray) -> The matrix
 def solution_to_matrix(df, nodes):
+    """
+    Get the decision variable x in matrix form
+    :param df: a Pandas dataframe that contains the solution of the model
+    :param nodes: the number of nodes
+    :return: the matrix that contains the solution
+    """
     matrix = numpy.zeros((nodes, nodes))
     for index, d in df.iterrows():
         pos = convert_row(d, nodes)
@@ -37,4 +74,3 @@ def solution_to_matrix(df, nodes):
         col = pos[1] - 1
         matrix[row][col] = 1
     return matrix
-
