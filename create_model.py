39f26525343dae1c032d8f34afebc9b923d088c2
marinimau <mauro.marini97@gmail.com>
2021-08-26 16:18:15 +0200

    refactoring

--- create_model.py
+++ create_model.py
@@ -35,23 +35,13 @@ def create_assignment_model(name, range_nodes, costs):
     :return:
     """
     m = Model(name=name, log_output=True)
-
     # Decision Variable
     x = m.binary_var_matrix(range_nodes, range_nodes)
-
     # In-degree and Out-degree of each vertex
-    for i in range_nodes:
-        m.add_constraint(m.sum(x[i, j] for j in range_nodes) == 1)
-    for j in range_nodes:
-        m.add_constraint(m.sum(x[i, j] for i in range_nodes) == 1)
-
+    [m.add_constraint(m.sum(x[i, j] for j in range_nodes) == 1) for i in range_nodes]
+    [m.add_constraint(m.sum(x[i, j] for i in range_nodes) == 1) for j in range_nodes]
     # No loop from the same node
-    for i in range_nodes:
-        for j in range_nodes:
-            if i == j:
-                m.add_constraint(x[i, j] == 0)
-
+    [m.add_constraint(x[i, i] == 0) for i in range_nodes]
     # Objective Function
     m.minimize(m.sum(costs[i][j] * x[i, j] for j in range_nodes for i in range_nodes))
-
     return m
